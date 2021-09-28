680ac5e18dd6de623a8ab7e2212afa4408f81e2b
chensu2436 <chensu@comp.nus.edu.sg>
2021-09-28 16:20:52 +0800

    first commit

---
+++ norm_test/norm_test.py
@@ -0,0 +1,101 @@
+"""
+Copyright 2019 ETH Zurich, Seonwook Park
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+#!/usr/bin/env python3
+
+import os
+import cv2
+import numpy as np
+import face
+from KalmanFilter1D import Kalman1D
+from normalisation import normalize
+import scipy.io as sio
+
+
+kalman_filters = list()
+kalman_filters.append(Kalman1D(sz=100, R=0.01 ** 2))
+kalman_filters_landm = list()
+for point in range(68):
+    # initialize Kalman filters for different coordinates
+    # will be used to smooth landmarks over the face for a single face tracking
+    kalman_filters_landm.append(Kalman1D(sz=100, R=0.005 ** 2))
+
+por = np.array([-127.790719, 4.621111, -12.025310]) # 3D gaze taraget position
+hr = np.array([[-0.11660857],[0.14517431],[-0.07825662]])
+ht = np.array([[11.53173266],[16.15314176],[431.66372868]])
+head_pose = (hr, ht)
+
+def detect_face(img):
+    # detect face
+    face_location = face.detect(img,  scale=0.25, use_max='SIZE')
+
+    if len(face_location) > 0:
+        # use kalman filter to smooth bounding box position
+        # assume work with complex numbers:
+        output_tracked = kalman_filters[0].update(face_location[0] + 1j * face_location[1])
+        face_location[0], face_location[1] = np.real(output_tracked), np.imag(output_tracked)
+        output_tracked = kalman_filters[1].update(face_location[2] + 1j * face_location[3])
+        face_location[2], face_location[3] = np.real(output_tracked), np.imag(output_tracked)
+
+        # detect facial points
+        pts = np.array([[551, 408], [603, 405], [698, 398], [755, 393], [603, 566], [724, 557]])
+        # run Kalman filter on landmarks to smooth them
+        for i in range(68):
+            kalman_filters_landm_complex = kalman_filters_landm[i].update(pts[i, 0] + 1j * pts[i, 1])
+            pts[i, 0], pts[i, 1] = np.real(kalman_filters_landm_complex), np.imag(kalman_filters_landm_complex)
+    return face_location
+
+
+if __name__ == '__main__':
+
+    print("reading camera param")
+    cameraCalib = sio.loadmat('../data/calibration/cameraCalib.mat')
+    camera_matrix = cameraCalib['cameraMatrix'] # shape (3,3)
+    camera_distortion = cameraCalib['distCoeffs']
+    fx, _, cx, _, fy, cy, _, _, _ = camera_matrix.flatten()
+    camera_parameters = np.asarray([fx, fy, cx, cy])
+
+    print("reading image")
+    filepath = os.path.join('../data/example/day01_0087.jpg')
+    img = cv2.imread(filepath)
+    img = cv2.undistort(img, camera_matrix, camera_distortion)
+
+    print("detecting face")
+    face_location = detect_face(img)
+
+    
+
+    entry = {
+            'full_frame': img,
+            '3d_gaze_target': por,
+            'camera_parameters': camera_parameters,
+            'full_frame_size': (img.shape[0], img.shape[1]),
+            'face_bounding_box': (int(face_location[0]), int(face_location[1]),
+                                    int(face_location[2] - face_location[0]),
+                                    int(face_location[3] - face_location[1]))
+            }
+    
+    print("normalizing")
+    [patch, h_n, g_n, inverse_M, gaze_cam_origin, gaze_cam_target] = normalize(entry, head_pose)
+
+    print("finish executing")
+    # cv2.imshow('raw patch', patch)
